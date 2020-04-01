626825266bebd5672eadce7baf70abfb6965ae4d
AliaksandrHaurylenka <goric0312@mail.ru>
2020-04-01 16:16:41 +0300

    asd

---
+++ webstudio-bootstrap/js/scripts.js
@@ -0,0 +1,8 @@
+$(function(){  
+    // Scroll Up
+    $.scrollUp({
+        topDistance: '300',
+        animation: 'fade',
+        scrollImg: { active: true, type: 'background', src: '/img/top.png' }
+    });
+});
