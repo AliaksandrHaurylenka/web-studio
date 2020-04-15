937250e85aa8cc3eba6a64bf8a1ea9e0d3e68b4c
AliaksandrHaurylenka <goric0312@mail.ru>
2020-04-15 16:29:02 +0300

    asd

--- web-studio-gulp/gulpfile.js
+++ web-studio-gulp/gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const pug2html = require('./gulp/tasks/pug2html');
 const styles = require('./gulp/tasks/styles');
+const imageMinify = require('./gulp/tasks/imageMinify');
 
-const dev = gulp.parallel(pug2html, styles);
+const dev = gulp.parallel(pug2html, styles, imageMinify);
 
 const build = gulp.series(dev);
 
