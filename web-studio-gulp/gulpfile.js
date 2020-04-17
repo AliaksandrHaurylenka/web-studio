9616a70f4c7b6e4b3750e1e18ece10b19b4975ea
Aliaksandr Haurylenka <aliaksandr.haurylenka@gmail.com>
2020-04-17 07:27:49 +0300

    asd

--- web-studio-gulp/gulpfile.js
+++ web-studio-gulp/gulpfile.js
@@ -2,10 +2,10 @@ const gulp = require('gulp');
 const pug2html = require('./gulp/tasks/pug2html');
 const styles = require('./gulp/tasks/styles');
 const imageMinify = require('./gulp/tasks/imageMinify');
+const fonts = require('./gulp/tasks/fonts');
 
-const dev = gulp.parallel(pug2html, styles, imageMinify);
+const dev = gulp.parallel(pug2html, styles, imageMinify, fonts);
 
 const build = gulp.series(dev);
 
 module.exports.build = build;
-
